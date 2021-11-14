5c0e2fb47bbf902fc56e7b072200182a22caad0c
mzalot <69329263+mzalot@users.noreply.github.com>
2021-11-14 14:49:27 -0700

    I added instance data and a default constructor to the Stack class

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -6,5 +6,14 @@
  Collaboration:
  Date: 11/8/21
  **/
-public class Stack {
+public class Stack <T>{
+    //instance data
+    private int counter;
+
+    //default constructor
+    public Stack(){
+        counter = 0;
+        StackNode node = new StackNode();
+    }
+    //methods
 }
